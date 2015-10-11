7a8d547c6432cd068b8c4980758c1be77d81492a
Yasmin Bey <yasminbey@hotmail.co.uk>
2015-10-11 18:00:30 +0100

    This code will only work in Python 2, due to a lack of a python 3 wiimote library. You will need to execute 'sudo apt-get install python-dev' and then 'sudo apt-get install spidev' on your pi in order to install the necessary libraries for GPIOzero. If you are running Raspbian Jessie then there is no need to download zero, however if you are using wheezy then you need to download the gpiozero through the terminal. Ben Nuttall has very good instructions on how to do this. You will also need a wiimote and bluetooth dongle that is already configured to use this code. Matthew Timmons-Brown has a very good tutorial on his youtube channel - TheRaspberryPiGuy.

---
+++ gpiozerowiimote.py
@@ -0,0 +1,40 @@
+# Wiimote GPIOZero code by Yasmin Bey
+from gpiozero import RyanteckRobot
+import cwiid, time
+robot = RyanteckRobot()
+
+button_delay = 0.01
+
+print("Hold down buttons 1 and 2 to start the connection process!")
+time.sleep(1)
+
+try:
+    wiimote = cwiid.Wiimote()
+except RuntimeError:
+    print("Failed connection. Please re-run the program!")
+    quit()
+
+print("Connection successful. Buttons will be active after 2 seconds")
+wiimote.rumble = 1 # This can be removed if you don't want the remote to rumble when connected!
+time.sleep(2)
+wiimote.rumble = 0
+
+wiimote.rpt_mode = cwiid.RPT_BTN
+
+while True:
+    buttons = wiimote.state["buttons"]
+    speed = 0.7 # This can be altered between 0 and 1, dependent on requirements.
+    if (buttons & cwiid.BTN_LEFT):
+        robot.left(speed)
+
+    if (buttons & cwiid.BTN_RIGHT):
+         robot.right(speed)
+
+    if (buttons & cwiid.BTN_UP):
+         robot.forward(speed)
+
+    if (buttons & cwiid.BTN_DOWN):
+         robot.backward(speed)
+
+    if (buttons & cwiid.BTN_B):
+         robot.stop()
