8497ab91693a74854761634ae96eaf8552ac65cd
RPi-Yaz14 <yasminbey@hotmail.co.uk>
2015-11-14 12:52:13 +0000

    Update linefollower.py

--- linefollower.py
+++ linefollower.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
-
+from gpiozero import RyanteckRobot
+robot = RyanteckRobot()
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -11,7 +12,10 @@ GPIO.setup(26, GPIO.IN) # Right
 while True:
         if GPIO.input(21) == GPIO.input(26):
                 print("forward")
+                robot.forward()
         if GPIO.input(21) != 0:
                 print("left")
+                robot.left()
         if GPIO.input(26) !=  0:
                 print("right")
+                robot.right()
